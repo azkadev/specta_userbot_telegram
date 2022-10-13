33322d922b60d11555b213330f895e5299044ea6
Your Name <you@example.com>
2022-10-13 18:47:39 +0700

    update

--- bin/specta_userbot_telegram.dart
+++ bin/specta_userbot_telegram.dart
@@ -1,5 +1,34 @@
-import 'package:specta_userbot_telegram/specta_userbot_telegram.dart' as specta_userbot_telegram;
+// ignore_for_file: non_constant_identifier_names, unused_local_variable
 
-void main(List<String> arguments) {
-  print('Hello world: ${specta_userbot_telegram.calculate()}!');
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:galaxeus_lib/galaxeus_lib.dart'; 
+
+void main(List<String> args) async {
+  String username = Platform.environment["username"] ?? "admin";
+  String password = Platform.environment["password"] ?? "azka123";
+  String host_name = Platform.environment["HOST_API"] ?? "wss://specta-apis.up.railway.app/ws";
+  WebSocketClient ws = WebSocketClient(host_name); 
+  ws.on("update", (update) {
+    try {
+      if (update is Map) {
+        if (update["@type"] is String == false) {
+          return;
+        }
+        String method = (update["@type"] as String);
+      }
+    } catch (e) {
+      print(e);
+    }
+  });
+  await ws.connect(
+    onDataUpdate: (data) {
+      if (data is String && data.isNotEmpty) {
+        try {
+          return ws.event_emitter.emit(ws.event_name_update, null, json.decode(data));
+        } catch (e) {}
+      }
+    },
+  );
 }
