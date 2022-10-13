065c8551b280fdb10ba856715cc25891811352e5
Your Name <you@example.com>
2022-10-13 07:30:02 +0700

    update

---
+++ bin/specta_userbot_telegram.dart
@@ -0,0 +1,5 @@
+import 'package:specta_userbot_telegram/specta_userbot_telegram.dart' as specta_userbot_telegram;
+
+void main(List<String> arguments) {
+  print('Hello world: ${specta_userbot_telegram.calculate()}!');
+}
